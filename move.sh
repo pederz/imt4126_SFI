3c6424d14c462e7c90382aa3187336187700be04
Espen Lien Pedersen <espenped@stud.ntnu.no>
2022-05-12 12:05:42 -0700

    Add files via upload

--- move.sh
+++ move.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
-for D in *.png; do
-    if [ -e "${D}" ]; then
-        #echo "${D}"
-	dir=$(cut -c 1-6 <<<"$D")
-	#echo "$dir"
-	if [ -e "$dir" ]
-	then
-	    mv "$D" "$dir"/
-	else
-	    mkdir "$dir"
-	    mv "$D" "$dir"/
-        fi
-    fi
-done
+if [[ "$PWD" = */generated_images ]]; then
+	if [ ! -d "../generated_npy" ]; then
+		mkdir "../generated_npy"
+	fi
+	echo "Folder is generated_images"
+	for file in *; do
+       		if [ -f $file ]; then
+                	if [[ $file == *.npy ]]; then
+                        	mv "$file" "../generated_npy/$file"
+                	elif [[ $file == *.png ]]; then
+                        	dir=$(cut -c 1-6 <<<"$file")
+				echo "$file"
+				echo "$dir"
+                        	if [ ! -e "$dir" ]; then
+					mkdir "$dir"
+                        	fi
+                                mv "$file" "$dir"/
+                	fi
+        	fi
+	done
+elif [[ "$PWD" = */aligned_images ]]; then
+	echo "Folder is aligned_images"
+	for file in *; do
+                if [ -f $file ]; then
+                        if [[ $file == *.png ]]; then
+                                dir=$(cut -c 1-6 <<<"$file")
+                                if [ ! -e "$dir" ]; then
+					mkdir "$dir"
+				fi
+                                mv "$file" "$dir"/
+                        fi
+                fi
+        done
+else
+	echo "You are not running the file in generated_iamges or aligned_images folder"
+fi
